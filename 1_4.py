57fcc8baea842d7104904e3c43b5aaf4cf8467f0
ViViSpiro <scientia@vivispiro.ru>
2022-06-22 22:13:56 +0300

    1_4 update

--- 1_4.py
+++ 1_4.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(-6, 10, 130)
-
-plt.plot(x, np.cos(1 * x))
-plt.plot(x, np.cos(2 * x))
+k_1 = 1
+k_2 = 2
+plt.plot(x, np.cos(k_1 * x))
+plt.plot(x, np.cos(k_2 * x))
 plt.show()
